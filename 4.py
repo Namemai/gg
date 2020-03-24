d164414bdf09c9a5c5be53c698d32e5f309085bf
Namemai <40319864+Namemai@users.noreply.github.com>
2020-03-24 10:47:18 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -53,10 +53,10 @@ print("---LOGIN SUCCES---\n mai")
 
 poll = LinePoll(cl)
 call = cl
-creator = ["uc66e45201d1612eb4ce7b3a86bac4685"]
-owner = ["uc66e45201d1612eb4ce7b3a86bac4685"]
-admin = ["uc66e45201d1612eb4ce7b3a86bac4685"]
-staff = ["uc66e45201d1612eb4ce7b3a86bac4685"]
+creator = ["uc66e45201d1612eb4ce7b3a86bac4685","ue19a299b94742f369cd1afe3d3f6f194]
+owner = ["uc66e45201d1612eb4ce7b3a86bac4685","ue19a299b94742f369cd1afe3d3f6f194]
+admin = ["uc66e45201d1612eb4ce7b3a86bac4685","ue19a299b94742f369cd1afe3d3f6f194]
+staff = ["uc66e45201d1612eb4ce7b3a86bac4685","ue19a299b94742f369cd1afe3d3f6f194]
 mid = cl.getProfile().mid
 Amid = ki.getProfile().mid
 Bmid = kk.getProfile().mid
